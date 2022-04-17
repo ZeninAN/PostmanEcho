c082a542f387f3cec9e238cdb1fd5bd4a2e1c20e
ZeninAN <95536097+ZeninAN@users.noreply.github.com>
2022-04-17 13:12:57 +0400

    Initial commit

---
+++ src/test/java/TestApi.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+public class TestApi {
+    @Test
+    void shouldReturnDemoAccounts() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+}
