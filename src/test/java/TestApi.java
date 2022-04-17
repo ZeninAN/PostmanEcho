0d14f2c4830cbac0ab97ffdec0dec4de31715e91
ZeninAN <95536097+ZeninAN@users.noreply.github.com>
2022-04-17 13:18:09 +0400

    test fail

--- src/test/java/TestApi.java
+++ src/test/java/TestApi.java
@@ -14,6 +14,6 @@ public class TestApi {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"));
+                .body("data", equalTo("value"));
     }
 }
